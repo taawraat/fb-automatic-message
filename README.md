9a66985e5c1c75960930ba7b4000e566faef1b71
taawraat <tawraat0187@gmail.com>
2020-04-30 17:22:26 +0600

    script added

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # fb-automatic-message
 A python script to send automatic message with selenium.
+
+Requirements:
+1.Python
+2.pip
+3.Selenium
+4.Chrome driver
+
+
+If you have python and pip installed in your device then follow below, else install python and pip first:
+
+goto this link and download gecko driver for your device
+
+https://github.com/mozilla/geckodriver/releases
+
+after downloading, extract the geckodriver.exe file and place it to the folder where you have installed python.In my case it's
+
+C:\Users\user\AppData\Local\Programs\Python\Python38
+
+After placing it,in your terminal,type the following command
+
+pip install selenium
+
+then run the script.
+
+Thats it.
+
---
+++ script.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from getpass import getpass
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# user input message id, ex: messenger.com/t/123123232.. Type only the number.
+message_to = input('message ID: ')
+
+# user input facebook login number.
+number = str(input('login number: '))
+
+# user input facebook login password
+pwd = str(getpass("login password: "))
+
+# type the message to send
+message = str(input("type what to send: "))
+
+# open the chrome browser
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+# goto messenger.com
+driver.get('https://m.me')
+
+# log in with given info
+driver.find_element_by_name('email').send_keys(number)
+driver.find_element_by_id('pass').send_keys(pwd)
+driver.find_element_by_id('loginbutton').click()
+time.sleep(2)
+
+# finding the given inbox
+driver.get(f'https://messenger.com/t/{message_to}')
+
+# sending the message
+driver.switch_to_active_element().send_keys(message)
+time.sleep(1)
+driver.switch_to_active_element().send_keys(Keys.ENTER)
+time.sleep(3)
+
+# done and quitting the browser
+driver.quit()
